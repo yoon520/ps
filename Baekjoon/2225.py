13cd7897c5e57aac6d9cee38bd1b1a179a8abc0e
yoon520 <gmlwjddbs123@naver.com>
2023-08-07 21:44:20 +0900

    DP - 합분해

---
+++ Baekjoon/2225.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+dp =[[1 for _ in range(n+1)] for _ in range(k)]
+
+for i in range(1, k):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+print(dp[-1][-1])
